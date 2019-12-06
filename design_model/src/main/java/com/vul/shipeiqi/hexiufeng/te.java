c7d4cc1d8a0a2c47304649edba1e8264dc7d717a
xiaoqianyong <xiaoqianyong@antiy.cn>
2019-12-06 11:38:01 +0800

    feat：建立包

---
+++ design_model/src/main/java/com/vul/shipeiqi/hexiufeng/te.java
@@ -0,0 +1,9 @@
+package com.vul.shipeiqi.hexiufeng;
+
+/**
+ * @Author xiaoqianyong
+ * @DATE 2019/12/6 11:34
+ * @Description
+ */
+public class te {
+}
---
+++ design_model/src/main/java/com/vul/shipeiqi/xiaoqianyong/Temp.java
@@ -0,0 +1,9 @@
+package com.vul.shipeiqi.xiaoqianyong;
+
+/**
+ * @Author xiaoqianyong
+ * @DATE 2019/12/6 11:33
+ * @Description
+ */
+public class Temp {
+}
